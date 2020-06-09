aa0b47f69ecce0276e705a98ddb1a6efbbd24c3a
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-09 19:04:51 +0500

    feat: add Vacancy Modal

---
+++ src/components/VacancyModal/VacancyModal.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {Modal} from "react-bootstrap";
+
+const VacancyModal = (props) => {
+
+	return (
+		<React.Fragment>
+			<Modal show={props.open} onHide={props.handleClose} centered size={"lg"}>
+				<Modal.Header closeButton>
+					<Modal.Title>
+						{props.data["job-name"]}
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<span className="d-inline-block mb-2 font-weight-light">{props.data.company.name}</span>
+					<p>
+						<b className="d-inline-block mb-1">Данные по вакансии:</b>
+						<br/>
+						<span className="mr-2">Сфера деятельности:</span><span>{props.data.category.specialisation}</span>
+						<br/>
+						<span className="mr-2">Зарплата:</span><span>{props.data.salary} {props.data.currency}</span>
+						<br/>
+						<span className="mr-2">График работы:</span><span>{props.data.schedule}</span>
+						<br/>
+						<span className="mr-2">Тип занятости:</span><span>{props.data.employment}</span>
+						{props.data.term ?
+							<React.Fragment><br/> <span className="mr-2">Бонусы:</span><span>{props.data.term.text}</span></React.Fragment>
+						: null}
+					</p>
+					<hr/>
+					<p>
+						<b className="d-inline-block mb-1">Обязанности:</b>
+						<br/>
+						<span>{(props.data.duty)}</span>
+					</p>
+					<hr/>
+					<p>
+						<b className="d-inline-block mb-1">Требования:</b>
+						<br/>
+						<span className="mr-2">Опыт работы:</span>
+						{props.data.requirement.experience === 0 ? <span>Без опыта</span> : <span>{props.data.requirement.experience} года</span>}
+						{/*ToDo: сделать склонения слова 'год' в зависимости от числа*/}
+						<br/>
+						<span className="mr-2">Образование:</span><span>{props.data.requirement.education}</span>
+						<br/>
+						<span className="mr-2">Профессиональные качества:</span><span>{props.data.requirement.qualification}</span>
+					</p>
+					<hr/>
+					<p>
+						<b className="d-inline-block mb-1">Контактная информация:</b>
+						<br/>
+						<span className="mr-2">Телефон:</span>
+						{props.data.company.phone ? <a href="#">{props.data.company.phone}</a> : <span>Не указан</span>}
+						<br/>
+						<span className="mr-2">E-mail:</span>
+						{props.data.company.email ? <a href="#">{props.data.company.email}</a> : <span>Не указан</span>}
+						<br/>
+						<span className="mr-2">Адрес:</span><span>{props.data.addresses.address["0"].location}</span>
+					</p>
+				</Modal.Body>
+			</Modal>
+		</React.Fragment>
+	)
+}
+
+export default VacancyModal
