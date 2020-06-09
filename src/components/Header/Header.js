d05c9821d0ae3d368e715ab64d4d35a56ad649fe
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-09 18:46:34 +0500

    feat: add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+	return (
+		<Navbar bg="dark" variant="dark">
+			<Navbar.Brand href="#home">
+				Поиск работы в РФ
+			</Navbar.Brand>
+		</Navbar>
+	)
+}
+
+export default Header
