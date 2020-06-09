b10854a2337ff8b2b891822109b05e15bac6e845
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-09 19:04:15 +0500

    feat: app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Layout from "./hoc/Layout/Layout";
+import Header from "./components/Header/Header";
+import SearchPage from "./containers/SearchPage/SearchPage";
+import {Switch, Route, Redirect} from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Layout>
+			<Header/>
+			<Switch>
+				<Route path={"/"} component={SearchPage}/>
+				<Route path={"*"}>
+					<Redirect to="/"/>
+				</Route>
+			</Switch>
+		</Layout>
+	);
 }
 
 export default App;
