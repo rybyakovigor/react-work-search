bd24913c4474ceb8a90da6eccc8d5792eaefb112
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-09 18:48:27 +0500

    feat: add search

---
+++ src/components/VacancySearchInput/VacancySearchInput.js
@@ -0,0 +1,25 @@
+import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+const VacancySearchInput = (props) => {
+
+	return (
+		<React.Fragment>
+			<InputGroup className="mb-3">
+				<FormControl
+					onChange={props.searchInput}
+					placeholder="Введите название вакансии"
+					aria-label="Поле для ввода вакансии"
+					aria-describedby="Поле для ввода вакансии"
+				/>
+				<InputGroup.Append>
+					<Button onClick={props.searchButton} variant="outline-secondary">Искать</Button>
+				</InputGroup.Append>
+			</InputGroup>
+		</React.Fragment>
+	)
+}
+
+export default VacancySearchInput
