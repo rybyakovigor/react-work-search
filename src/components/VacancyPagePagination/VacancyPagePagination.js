aa50adf7a92fd8aa91202de4fb19d1be6bcf988a
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-11 18:34:55 +0500

    feat: add pagination

---
+++ src/components/VacancyPagePagination/VacancyPagePagination.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Pagination from "react-bootstrap/Pagination";
+
+const VacancyPagePagination = (props) => {
+
+	let items = [];
+
+	for (let number = 1; number <= Math.ceil(props.data.length / 10) ; number++) {
+		items.push(
+			<Pagination.Item onClick={props.click} id={number} key={number} active={number === props.currentPage}>
+				{number}
+			</Pagination.Item>,
+		);
+	}
+
+	const paginationBasic = (
+		<div>
+			<Pagination className="justify-content-end">{items}</Pagination>
+		</div>
+	);
+
+	return (
+		<div>
+			{paginationBasic}
+		</div>
+	)
+}
+
+export default VacancyPagePagination
--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -3,6 +3,7 @@ import VacancySearchInput from "../../components/VacancySearchInput/VacancySearc
 import axiosConfig from "../../axiosConfig";
 import VacancyCard from "../../components/VacancyCard/VacancyCard";
 import VacancyModal from "../../components/VacancyModal/VacancyModal";
+import VacancyPagePagination from "../../components/VacancyPagePagination/VacancyPagePagination";
 
 export default class SearchPage extends Component {
 
@@ -11,7 +12,17 @@ export default class SearchPage extends Component {
 		vacancy: [],
 		vacancyDetail: null,
 		region: 6600000000000,
-		vacancyOpen: false
+		vacancyOpen: false,
+		// Параметры для пагинации
+		vacancyOnPage: [],
+		currentPage: 1,
+		vacancyPerPage: 10
+	}
+
+	paginationClickHandler = (event) => {
+		this.setState({
+			currentPage: Number(event.target.id)
+		})
 	}
 
 	modalOpenHandler = (data) => {
@@ -33,7 +44,8 @@ export default class SearchPage extends Component {
 			let vacancy = []
 			vacancy.push(...response.data.results.vacancies)
 			this.setState({
-				vacancy: vacancy
+				vacancy: vacancy,
+				currentPage: 1
 			})
 		} catch (error) {
 			console.log(error)
@@ -65,21 +77,66 @@ export default class SearchPage extends Component {
 		})
 	}
 
+	renderVacancyPerPage() {
+		const vacancy = this.state.vacancy
+		const indexOfLastVacancy = this.state.currentPage * this.state.vacancyPerPage
+		const indentOfFirstVacancy = indexOfLastVacancy - this.state.vacancyPerPage
+		const currentVacancy = vacancy.slice(indentOfFirstVacancy, indexOfLastVacancy)
+
+		return currentVacancy.map((vacancy) => {
+			return (
+				<VacancyCard
+					key={vacancy.vacancy.id}
+					data={vacancy.vacancy}
+					openVacancy={() => {
+						console.log(vacancy.vacancy)
+						this.setState({
+							vacancyDetail: vacancy.vacancy,
+							vacancyOpen: true
+						})
+						console.log(this.state.vacancyDetail)
+					}}
+				/>
+			)
+		})
+	}
+
 	render() {
 		return (
-			<div className="pt-3 pb-3">
+			<div className="pt-3 pb-3 SearchPage">
 				<VacancySearchInput
 					searchInput={this.searchInputHandler}
 					searchButton={this.searchButtonHandler}
 				/>
-				{this.renderVacancyCards()}
+				{
+					this.state.vacancy.length > 10
+						?
+						<VacancyPagePagination
+							data={this.state.vacancy}
+							currentPage={this.state.currentPage}
+							click={this.paginationClickHandler}
+						/>
+						: null
+				}
+
+				{this.renderVacancyPerPage()}
+
 				{this.state.vacancyDetail ? <VacancyModal
 					open={this.state.vacancyOpen}
 					handleClose={this.modalCloseHandler}
 					handleShow={this.modalOpenHandler}
 					data={this.state.vacancyDetail}
 				/> : null}
-
+				{
+					this.state.vacancy.length > 10
+						?
+						<VacancyPagePagination
+							data={this.state.vacancy}
+							currentPage={this.state.currentPage}
+							click={this.paginationClickHandler}
+						/>
+						: null
+				}
 			</div>
 		)
 	}
