6d3e76480885cf63ba55a5c31a907960b8c4acde
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-14 01:49:40 +0500

    feat: change text position

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 const Footer = () => {
 
 	return (
-		<Navbar sticky="bottom" bg="dark" variant="dark" className="justify-content-center">
+		<Navbar sticky="bottom" bg="dark" variant="dark" className="footer flex-column flex-sm-row justify-content-center">
 			<span className="text-muted">Вакансии предоставлены сайтом</span>
 			<a target="_blank" href="https://trudvsem.ru/" rel="noopener noreferrer">&nbsp;https://trudvsem.ru/</a>
 		</Navbar>
