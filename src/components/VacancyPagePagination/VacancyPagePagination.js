15a3f4a6fc5c1dfe19da566db28a1fae7fa28a84
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-14 01:52:15 +0500

    feat: change pagination position on xs breakpoint

--- src/components/VacancyPagePagination/VacancyPagePagination.js
+++ src/components/VacancyPagePagination/VacancyPagePagination.js
@@ -5,7 +5,7 @@ const VacancyPagePagination = (props) => {
 
 	let items = [];
 
-	for (let number = 1; number <= Math.ceil(props.data.length / 10) ; number++) {
+	for (let number = 1; number <= Math.ceil(props.data.length / 10); number++) {
 		items.push(
 			<Pagination.Item onClick={props.click} id={number} key={number} active={number === props.currentPage}>
 				{number}
@@ -15,7 +15,7 @@ const VacancyPagePagination = (props) => {
 
 	const paginationBasic = (
 		<div>
-			<Pagination className="justify-content-end">{items}</Pagination>
+			<Pagination className="justify-content-center justify-content-sm-end">{items}</Pagination>
 		</div>
 	);
 
