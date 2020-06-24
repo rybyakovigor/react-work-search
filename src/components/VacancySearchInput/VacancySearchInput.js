63907f2e61a65bba16c56747247cd53350e2bb28
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-24 15:39:21 +0500

    style(VacancySearchInput): format code

--- src/components/VacancySearchInput/VacancySearchInput.js
+++ src/components/VacancySearchInput/VacancySearchInput.js
@@ -4,22 +4,24 @@ import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
 const VacancySearchInput = (props) => {
+  return (
+    <React.Fragment>
+      <InputGroup className="mb-3">
+        <FormControl
+          onKeyPress={props.keyHandler}
+          onChange={props.searchInput}
+          placeholder="Введите название вакансии"
+          aria-label="Поле для ввода вакансии"
+          aria-describedby="Поле для ввода вакансии"
+        />
+        <InputGroup.Append>
+          <Button onClick={props.searchButton} variant="outline-secondary">
+            Искать
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </React.Fragment>
+  );
+};
 
-	return (
-		<React.Fragment>
-			<InputGroup className="mb-3">
-				<FormControl
-					onChange={props.searchInput}
-					placeholder="Введите название вакансии"
-					aria-label="Поле для ввода вакансии"
-					aria-describedby="Поле для ввода вакансии"
-				/>
-				<InputGroup.Append>
-					<Button onClick={props.searchButton} variant="outline-secondary">Искать</Button>
-				</InputGroup.Append>
-			</InputGroup>
-		</React.Fragment>
-	)
-}
-
-export default VacancySearchInput
+export default VacancySearchInput;
