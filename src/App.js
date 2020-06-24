73172bf7309fe1a77330b2aceeeefa827975b233
Igor Rybyakov <rybyakovigor@gmail.com>
2020-06-24 15:17:34 +0500

    style(App): format code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./hoc/Layout/Layout";
 import Header from "./components/Header/Header";
 import SearchPage from "./containers/SearchPage/SearchPage";
-import {Switch, Route, Redirect} from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-	return (
-		<Layout>
-			<Header/>
-			<Switch>
-				<Route path={"/"} component={SearchPage}/>
-				<Route path={"*"}>
-					<Redirect to="/"/>
-				</Route>
-			</Switch>
-			<Footer/>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Header />
+      <Switch>
+        <Route path={"/"} component={SearchPage} />
+        <Route path={"*"}>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+      <Footer />
+    </Layout>
+  );
 }
 
 export default App;
