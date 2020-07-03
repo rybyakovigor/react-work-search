7557539ca34ab9d9406afc73c84abe58be1d07a4
Igor Rybyakov <rybyakovigor@gmail.com>
2020-07-03 13:44:01 +0500

    feat(index.js):  add redux imports, store, redux_devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter} from "react-router-dom"
-import {createStore} from "redux"
+import {createStore, compose, applyMiddleware} from "redux"
 import {Provider} from "react-redux"
+import thunk from 'redux-thunk'
 import reducer from "./store/reducers/rootReducer"
 import "./index.scss"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
-const store = createStore(reducer)
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+)
 
 const app = (
   <Provider store={store}>
